1fb044e18cd56fdea3591901e1d1e1e1e8b9e9bb
Nikita Zyuzin <nzyuzin93@gmail.com>
2016-02-06 15:22:36 +0400

    Fix case when a server is seen for the first time
    In this case we first try to read its state and return 0 if state is not found
    After this commit if old server state was not found then 1 is returned
    Motivation: When we see server for the first time there is not state recorded
    yet. After read returns 1 then write occurs which writes the state. If read
    returned 0 no write would ever occur.

--- src/server.c
+++ src/server.c
@@ -38,8 +38,10 @@ int read_server_state(struct server_state *state, char *server_name)
 	if (!(path = get_path(server_name, "state")))
 		return 0;
 	if (!(file = fopen(path, "r"))) {
-		if (errno != ENOENT)
-			perror(path);
+		if (errno == ENOENT)
+			/* State file has not been created yet */
+			return 1;
+		perror(path);
 		return 0;
 	}
 

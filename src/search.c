3b420ac5e87f53c7f4eb6bd9d3a4564d12fe13b9
Needs <needs@mailoo.org>
2016-03-08 18:05:43 +0100

    search: Do not display more than 50 results
    Since we have no more than 50 results, we can just use an array of 50
    results and remove/add results during the process.
    
    Well, in fact we use a linked-list backed by a pool of 50 results so
    adding/removing results are in O(1).  The code is a little bit complex
    but we can now handle almost any queries.  So this commit enable search
    for queries with 1 and 2 characters.
    
    This commit also add code to sort results by their relevance.  If the
    query match the beginning (or the end) of a player name, the it will be
    at the top of the list.
    
    We will add more rules later to have even better relevent results.

--- src/search.c
+++ src/search.c
@@ -4,12 +4,178 @@
 #include <errno.h>
 #include <string.h>
 #include <limits.h>
+#include <assert.h>
 
 #include "config.h"
 #include "io.h"
 #include "player.h"
 
-static void search(char *hex, struct player_array *array)
+/*
+ * Too much results is meanless, so by limiting the number of results we can
+ * statically allocate the result array.
+ */
+#define MAX_RESULTS 50
+
+/*
+ * Results are linked together, so that way, removing or adding a result is
+ * almost a O(1).
+ */
+struct result {
+	char name[MAX_NAME_LENGTH];
+	int score;
+
+	struct result *next, *prev;
+};
+
+/*
+ * The pool just act as a storage for the element of the linked-list. Results
+ * are sorted by their score.
+ *
+ * The list maintain a pointer to the last result so we can avoid searching
+ * when the result's score is lower than last result score.  And also because
+ * when the list is full and we want to add a new result, we then remove the
+ * last one.
+ */
+struct list {
+	unsigned length;
+	struct result pool[MAX_RESULTS];
+	struct result *first;
+	struct result *last;
+};
+
+/*
+ * The higher the score is, the better the name match the query. A score of
+ * zero means the result will be ignored.
+ */
+static unsigned get_score(char *name, char *query)
+{
+	unsigned score;
+	char *tmp;
+
+	if (!(tmp = strstr(name, query)))
+		return 0;
+
+	/*
+	 * Since name and query are in hexadecimal, each charcaters is two
+	 * bytes wide, so make sure we haven't matched two overlapping chars.
+	 */
+	if ((tmp - name) % 2 == 1)
+		return 0;
+
+	score = 10;
+
+	/* A query matching the beginning of a name is relevant */
+	if (tmp == name)
+		score += 20;
+
+	/* A query matching the end of a name is relevant as well */
+	if (tmp == name + strlen(name) - strlen(query) - 2)
+		score += 15;
+
+	/*
+	 * Let's not that if a query match exactly the name, both conditions
+	 * above will be matched and the score will be 50.
+	 */
+	return score;
+}
+
+static int is_empty(struct list *list)
+{
+	return list->length == 0;
+}
+
+static int is_full(struct list *list)
+{
+	return list->length == MAX_RESULTS;
+}
+
+/*
+ * Take a result from the pool if the pool is not full, otherwise remove
+ * the last result from the list, reset it's content and return it.
+ */
+static struct result *new_result(struct list *list, unsigned score, char *name)
+{
+	struct result *result;
+
+	if (!is_full(list)) {
+		/* Just return the next result from the pool */
+		result = &list->pool[list->length++];
+	} else {
+		/* The list is full, free the last entry and return it */
+		result = list->last;
+		list->last = result->prev;
+
+		result->prev->next = NULL;
+		result->prev = NULL;
+		result->next = NULL;
+	}
+
+	result->score = score;
+	strcpy(result->name, name);
+	return result;
+}
+
+/*
+ * Insert 'result' before 'target'.  If target is NULL, add the result at the
+ * end of the list.
+ */
+static void insert_before(struct list *list, struct result *target, struct result *result)
+{
+	assert(list != NULL);
+	assert(result != NULL);
+
+	if (target == list->first)
+		list->first = result;
+
+	if (target) {
+		result->prev = target->prev;
+		result->next = target;
+		if (target->prev)
+			target->prev->next = result;
+		target->prev = result;
+	} else {
+		/* Insert at the end */
+		result->prev = list->last;
+		result->next = NULL;
+		if (list->last)
+			list->last->next = result;
+		list->last = result;
+	}
+}
+
+static void try_add_result(struct list *list, unsigned score, char *name)
+{
+	struct result *result;
+
+	assert(list != NULL);
+
+	if (score == 0)
+		return;
+
+	if (is_empty(list))
+		return insert_before(list, NULL, new_result(list, score, name));
+
+	/*
+	 * We know the list is not empty and therefor list->last is set.
+	 * If the list is not full just add the result at the end.
+	 */
+	if (score < list->last->score) {
+		if (is_full(list))
+			return;
+		else
+			return insert_before(list, NULL, new_result(list, score, name));
+	}
+
+	/*
+	 * At this point we know the result should be in the list, but we have
+	 * to compute where to place it.
+	 */
+	for (result = list->first; result; result = result->next)
+		if (result->score <= score)
+			return insert_before(list, result, new_result(list, score, name));
+}
+
+static void search(char *hex, struct list *list)
 {
 	DIR *dir;
 	struct dirent *dp;
@@ -26,27 +192,25 @@ static void search(char *hex, struct player_array *array)
 	}
 
 	while ((dp = readdir(dir))) {
+		unsigned score;
+
 		if (!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, ".."))
 			continue;
-		if (strstr(dp->d_name, hex)) {
-			struct player player;
 
-			if (!read_player(&player, dp->d_name))
-				continue;
-			add_player(array, &player);
-		}
+		score = get_score(dp->d_name, hex);
+		try_add_result(list, score, dp->d_name);
 	}
 
 	closedir(dir);
 }
 
-static struct player_array PLAYER_ARRAY_ZERO;
+static struct list LIST_ZERO;
 
 int main(int argc, char **argv)
 {
 	static char hex[MAX_NAME_LENGTH];
-	struct player_array array = PLAYER_ARRAY_ZERO;
-	int i;
+	struct list list = LIST_ZERO;
+	size_t length;
 
 	load_config();
 
@@ -55,21 +219,29 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
-	if (strlen(argv[1]) < MAX_NAME_LENGTH && strlen(argv[1]) >= 3) {
+	/* No need to search when the query is too long or empty */
+	length = strlen(argv[1]);
+	if (length > 0 && length < MAX_NAME_LENGTH) {
 		string_to_hex(argv[1], hex);
-		search(hex, &array);
+		hex[strlen(hex) - 2] = '\0';
+		search(hex, &list);
 	}
 
 	CUSTOM_TAB.name = "Search results";
 	CUSTOM_TAB.href = "";
 	print_header(&CUSTOM_TAB, "Search results", argv[1]);
 
-	if (array.length == 0) {
+	if (list.length == 0) {
 		printf("No players found");
 	} else {
+		struct result *result;
+
 		printf("<table><thead><tr><th></th><th>Name</th><th>Clan</th><th>Score</th></tr></thead><tbody>\n");
-		for (i = 0; i < array.length && i < 100; i++)
-			html_print_player(&array.players[i], 1);
+		for (result = list.first; result; result = result->next) {
+			struct player player;
+			read_player(&player, result->name);
+			html_print_player(&player, 1);
+		}
 		printf("</tbody></table>");
 	}
 

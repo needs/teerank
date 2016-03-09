2375e70070c54694f6b1bef9063b41d2c1d83308
Needs <needs@mailoo.org>
2016-03-09 17:33:15 +0100

    search: Sort results by rank
    It seems to be a simple addition but in fact it is more complicated than
    it seems.
    
    We need to load the player, but we don't want to load every players.  So
    we only load the player if he may belongs to the final results.
    
    Also this commit fix some bugs as well, but I'm too put them in a
    separate commit anyway.  One of them was that to_lowercase() was not
    setting the terminating nul byte at the end of the destination string.

--- src/search.c
+++ src/search.c
@@ -18,28 +18,42 @@
 #define MAX_RESULTS 50
 
 /*
+ * Result contains the player struct because we sort result by relevance and ELO,
+ * so we need to store player's data at some point.
+ *
  * Results are linked together, so that way, removing or adding a result is
  * almost a O(1).
  */
 struct result {
 	char name[MAX_NAME_LENGTH];
-	int score;
+	int relevance;
+
+	int is_loaded;
+	struct player player;
 
 	struct result *next, *prev;
 };
 
 /*
  * The pool just act as a storage for the element of the linked-list. Results
- * are sorted by their score.
+ * are sorted by their relevance.
+ *
+ * The pool does contain one extra result to avoid an extra copy when adding
+ * a new result.  The adding process is as follow:
+ *   - Get a temporary result (the extra result pointed by "free")
+ *   - If it belongs to the list, just link it
+ *   - If it does not, do nothing
  *
  * The list maintain a pointer to the last result so we can avoid searching
- * when the result's score is lower than last result score.  And also because
+ * when the result's relevance is lower than last result relevance.  And also because
  * when the list is full and we want to add a new result, we then remove the
  * last one.
  */
 struct list {
 	unsigned length;
-	struct result pool[MAX_RESULTS];
+	struct result pool[MAX_RESULTS + 1];
+	struct result *free;
+
 	struct result *first;
 	struct result *last;
 };
@@ -53,15 +67,16 @@ static void to_lowercase(char *src, char *dst)
 
 	for (i = 0; src[i] != '\0'; i++)
 		dst[i] = tolower(src[i]);
+	dst[i] = '\0';
 }
 
 /*
- * The higher the score is, the better the name match the query. A score of
+ * The higher the relevance is, the better the name match the query. A relevance of
  * zero means the result will be ignored.
  */
-static unsigned get_score(char *hex, char *query)
+static unsigned get_relevance(char *hex, char *query)
 {
-	unsigned score;
+	unsigned relevance;
 	char *tmp;
 	char name[MAX_NAME_LENGTH];
 
@@ -72,21 +87,29 @@ static unsigned get_score(char *hex, char *query)
 	if (!(tmp = strstr(name, query)))
 		return 0;
 
-	score = 10;
+	relevance = 10;
 
 	/* A query matching the beginning of a name is relevant */
 	if (tmp == name)
-		score += 20;
+		relevance += 20;
 
 	/* A query matching the end of a name is relevant as well */
 	if (tmp == name + strlen(name) - strlen(query))
-		score += 15;
+		relevance += 15;
 
 	/*
-	 * Let's not that if a query match exactly the name, both conditions
-	 * above will be matched and the score will be 50.
+	 * Let's note that if a query match exactly the name, both conditions
+	 * above will be matched and the relevance will be 50.
 	 */
-	return score;
+	return relevance;
+}
+
+static void init_list(struct list *list)
+{
+	list->length = 0;
+	list->first = NULL;
+	list->last = NULL;
+	list->free = &list->pool[0];
 }
 
 static int is_empty(struct list *list)
@@ -100,89 +123,119 @@ static int is_full(struct list *list)
 }
 
 /*
- * Take a result from the pool if the pool is not full, otherwise remove
- * the last result from the list, reset it's content and return it.
+ * Just use the list->free pointer and initialize it.
  */
-static struct result *new_result(struct list *list, unsigned score, char *name)
+static struct result *new_result(struct list *list, unsigned relevance, char *name)
 {
-	struct result *result;
-
-	if (!is_full(list)) {
-		/* Just return the next result from the pool */
-		result = &list->pool[list->length++];
-	} else {
-		/* The list is full, free the last entry and return it */
-		result = list->last;
-		list->last = result->prev;
-
-		result->prev->next = NULL;
-		result->prev = NULL;
-		result->next = NULL;
-	}
-
-	result->score = score;
-	strcpy(result->name, name);
-	return result;
+	list->free->relevance = relevance;
+	strcpy(list->free->name, name);
+	list->free->is_loaded = 0;
+	return list->free;
 }
 
 /*
- * Insert 'result' before 'target'.  If target is NULL, add the result at the
- * end of the list.
+ * Insert 'result' before 'target'.  Remove the last result if the list is full.
+ * If target is NULL, insert the result at the end of the list.
  */
 static void insert_before(struct list *list, struct result *target, struct result *result)
 {
 	assert(list != NULL);
 	assert(result != NULL);
 
-	if (target == list->first)
-		list->first = result;
-
-	if (target) {
+	if (!target) {
+		if (is_empty(list)) {
+			/* First result in the list */
+			result->prev = NULL;
+			result->next = NULL;
+			list->first = result;
+			list->last = result;
+		} else {
+			/* Append the result */
+			result->prev = list->last;
+			result->next = NULL;
+			list->last->next = result;
+			list->last = result;
+		}
+	} else {
+		/* Insert before target */
 		result->prev = target->prev;
 		result->next = target;
-		if (target->prev)
-			target->prev->next = result;
 		target->prev = result;
+
+		if (target == list->first) {
+			list->first = result;
+			result->prev = NULL;
+		} else {
+			result->prev->next = result;
+		}
+	}
+
+	/* Update the list->free pointer */
+	if (is_full(list)) {
+		list->free = list->last;
+		list->last = list->last->prev;
+		list->last->next = NULL;
 	} else {
-		/* Insert at the end */
-		result->prev = list->last;
-		result->next = NULL;
-		if (list->last)
-			list->last->next = result;
-		list->last = result;
+		list->length++;
+		list->free = &list->pool[list->length];
 	}
 }
 
-static void try_add_result(struct list *list, unsigned score, char *name)
+static int get_rank(struct result *result)
 {
-	struct result *result;
+	if (!result->is_loaded) {
+		read_player(&result->player, result->name);
+		result->is_loaded = 1;
+	}
+
+	return result->player.rank;
+}
+
+static int cmp_results(struct result *a, struct result *b)
+{
+	if (a->relevance < b->relevance)
+		return -1;
+	else if (a->relevance > b->relevance)
+		return 1;
+	else if (get_rank(a) > get_rank(b))
+		return -1;
+	else if (get_rank(a) < get_rank(b))
+		return 1;
+	else
+		return 0;
+}
+
+static void try_add_result(struct list *list, unsigned relevance, char *name)
+{
+	struct result *result, *r;
 
 	assert(list != NULL);
 
-	if (score == 0)
+	if (relevance == 0)
 		return;
 
+	result = new_result(list, relevance, name);
+
 	if (is_empty(list))
-		return insert_before(list, NULL, new_result(list, score, name));
+		return insert_before(list, NULL, result);
 
 	/*
 	 * We know the list is not empty and therefor list->last is set.
 	 * If the list is not full just add the result at the end.
 	 */
-	if (score < list->last->score) {
-		if (is_full(list))
-			return;
-		else
-			return insert_before(list, NULL, new_result(list, score, name));
+	if (cmp_results(list->last, result) > 0) {
+		if (!is_full(list))
+			insert_before(list, NULL, result);
+		return;
 	}
 
 	/*
-	 * At this point we know the result should be in the list, but we have
-	 * to compute where to place it.
+	 * Insert the result in a sorted manner
 	 */
-	for (result = list->first; result; result = result->next)
-		if (result->score <= score)
-			return insert_before(list, result, new_result(list, score, name));
+	for (r = list->first; r; r = r->next)
+		if (cmp_results(r, result) < 0)
+			break;
+	insert_before(list, r, result);
 }
 
 static void search(char *query, struct list *list)
@@ -203,15 +256,16 @@ static void search(char *query, struct list *list)
 	}
 
 	to_lowercase(query, lowercase_query);
+	init_list(list);
 
 	while ((dp = readdir(dir))) {
-		unsigned score;
+		unsigned relevance;
 
 		if (!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, ".."))
 			continue;
 
-		score = get_score(dp->d_name, lowercase_query);
-		try_add_result(list, score, dp->d_name);
+		relevance = get_relevance(dp->d_name, lowercase_query);
+		try_add_result(list, relevance, dp->d_name);
 	}
 
 	closedir(dir);
@@ -247,9 +301,9 @@ int main(int argc, char **argv)
 
 		printf("<table><thead><tr><th></th><th>Name</th><th>Clan</th><th>Score</th></tr></thead><tbody>\n");
 		for (result = list.first; result; result = result->next) {
-			struct player player;
-			read_player(&player, result->name);
-			html_print_player(&player, 1);
+			if (!result->is_loaded)
+				read_player(&result->player, result->name);
+			html_print_player(&result->player, 1);
 		}
 		printf("</tbody></table>");
 	}

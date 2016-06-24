29d4d12ec23329512a671be354431df10f0e152c
Needs <needs@mailoo.org>
2016-06-24 03:56:05 +0200

    compute-rank: use player_summary structures for sorting
    Hence avoid using player_array while making things faster.  It also
    remove a bug where reallocated player's structure historics doesn't
    works because their internal pointer are invalidated by realloc().
    
    It was the last use a player_array in the code base, we are ready to
    remove it.

--- src/compute-ranks.c
+++ src/compute-ranks.c
@@ -4,54 +4,128 @@
 #include <string.h>
 #include <dirent.h>
 #include <limits.h>
+#include <errno.h>
 
 #include "config.h"
 #include "player.h"
 
-static void load_players(struct player_array *array)
+static int ignored;
+
+static void ignore_newest_player(void)
+{
+	ignored = 1;
+}
+
+static struct player_summary *new_player(
+	struct player_summary **_players, unsigned *_nplayers)
+{
+	static const unsigned STEP = 1024 * 1024;
+	struct player_summary *players = *_players;
+	unsigned nplayers = *_nplayers;
+
+	if (ignored) {
+		ignored = 0;
+		return &players[nplayers - 1];
+	}
+
+	if (nplayers % STEP == 0) {
+		struct player_summary *tmp;
+
+		tmp = realloc(players, (nplayers + STEP) * sizeof(*players));
+		if (!tmp) {
+			fprintf(stderr, "realloc(%u): %s\n",
+			        nplayers, strerror(errno));
+			exit(EXIT_FAILURE);
+		}
+		players = tmp;
+	}
+
+	*_players = players;
+	*_nplayers = nplayers + 1;
+
+	return &players[nplayers];
+}
+
+static struct player_summary *load_all_players(unsigned *nplayers)
 {
 	DIR *dir;
 	struct dirent *dp;
 	static char path[PATH_MAX];
-	struct player player;
 
-	assert(array != NULL);
+	struct player_summary *players = NULL;
+
+	assert(nplayers != NULL);
+
+	*nplayers = 0;
 
 	sprintf(path, "%s/players", config.root);
 	if (!(dir = opendir(path)))
 		perror(path), exit(EXIT_FAILURE);
 
-	init_player(&player);
 	while ((dp = readdir(dir))) {
+		struct player_summary *player;
+
 		if (!strcmp(dp->d_name, ".") || !strcmp(dp->d_name, ".."))
 			continue;
 		if (!is_valid_hexname(dp->d_name))
 			continue;
 
-		if (!read_player(&player, dp->d_name))
+		player = new_player(&players, nplayers);
+		if (!read_player_summary(player, dp->d_name)) {
+			ignore_newest_player();
 			continue;
-		add_player(array, &player);
+		}
 	}
 
 	closedir(dir);
+
+	return players;
 }
 
-static int cmp_player(const void *p1, const void *p2)
+static int cmp_players_elo(const void *p1, const void *p2)
 {
-	const struct player *a = p1, *b = p2;
+	const struct player_summary *a = p1, *b = p2;
 
 	/* We want them in reverse order */
 	return b->elo - a->elo;
 }
 
-static const struct player_array PLAYER_ARRAY_ZERO;
-
-int main(int argc, char *argv[])
+static void write_ranks(struct player_summary *players, unsigned nplayers)
 {
-	struct player_array array = PLAYER_ARRAY_ZERO;
 	unsigned i;
 	FILE *file;
 	char path[PATH_MAX];
+
+	struct player player;
+
+	sprintf(path, "%s/ranks", config.root);
+	if (!(file = fopen(path, "w"))) {
+		perror(path);
+		exit(EXIT_FAILURE);
+	}
+
+	/* Print the number of players first... */
+	fprintf(file, "%u players\n", nplayers);
+
+	/* ...and print player's name, and then save player infos themself. */
+	init_player(&player);
+	for (i = 0; i < nplayers; i++) {
+		fprintf(file, "%s\n", players[i].name);
+
+		if (!read_player(&player, players[i].name))
+			continue;
+
+		set_rank(&player, i + 1);
+		write_player(&player);
+	}
+
+	fclose(file);
+}
+
+int main(int argc, char *argv[])
+{
+	unsigned nplayers;
+	struct player_summary *players;
 
 	load_config();
 	if (argc != 1) {
@@ -59,26 +133,10 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	load_players(&array);
-	qsort(array.players, array.length, sizeof(*array.players), cmp_player);
+	players = load_all_players(&nplayers);
+	qsort(players, nplayers, sizeof(*players), cmp_players_elo);
 
-	sprintf(path, "%s/ranks", config.root);
-	if (!(file = fopen(path, "w")))
-		return perror(path), EXIT_FAILURE;
-
-	/* Print the number of players first... */
-	fprintf(file, "%u players\n", array.length);
-
-	/* ...and print player's name, and then save player infos themself. */
-	for (i = 0; i < array.length; i++) {
-		struct player *player = &array.players[i];
-
-		fprintf(file, "%s\n", player->name);
-		set_rank(player, i + 1);
-		write_player(player);
-	}
-
-	fclose(file);
+	write_ranks(players, nplayers);
 
 	return EXIT_SUCCESS;
 }

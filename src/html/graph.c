380444229a3819289b759d31bbbc40c051eaed43
Needs <needs@mailoo.org>
2016-06-25 04:27:15 +0200

    graph: Set a default font family
    Firefox default font was OK, but chrome one didn't fit the current style.

--- src/html/graph.c
+++ src/html/graph.c
@@ -371,7 +371,7 @@ static void print_graph(
 	graph = init_graph(hist, to_long);
 
 	svg("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>");
-	svg("<svg version=\"1.1\" baseProfile=\"full\" xmlns=\"http://www.w3.org/2000/svg\">");
+	svg("<svg version=\"1.1\" baseProfile=\"full\" xmlns=\"http://www.w3.org/2000/svg\" style=\"font-family: Verdana,Arial,Helvetica,sans-serif;\">");
 
 	if (graph.is_empty) {
 		print_notice_empty(&graph);

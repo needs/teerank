f32f19efc0529ed6bbdd2eef129e58afdbf1a0c6
Needs <needs@mailoo.org>
2016-07-09 19:28:53 +0200

    Make a upgrade before upgrading
    The rational behind this change is that when an upgrade fail, database
    is left in an unknown state.  It means that re-doing an upgrade on it
    will probably not work.  So if the user did not make a backup before
    upgrading, he or she is screwed.
    
    Instead of asking the user to make a backup and waiting for his
    confirmation, we just take the initiative to make a fast backup and then
    restore the database on failure.
    
    Backup is cleaned at exit to keep the directory clean.
    
    This commit does also remove the redicrection of echo to standard
    error.  before teerank-upgrade was not designed as an interactive tool,
    but now that it require user input in some situation, it is an
    interactive tool.  Hence message should be send to standard output
    because they are part of the interface.

--- builtin/upgrade.sh
+++ builtin/upgrade.sh
@@ -8,25 +8,48 @@ else
 	version=0
 fi
 
+if (( version == $DATABASE_VERSION )); then
+	echo "Database already upgraded to the latest version ($version)"
+	exit 0
+fi
+
 if [ $version -lt $DATABASE_VERSION ] && [ $STABLE_VERSION -eq 0 ]; then
-	echo "Warning: data are being upgraded to an unstable database format" 1>&2
-	echo "         this format may not be portable to newer stable release" 1>&2
-	echo -n "Type \"continue\" to proceed: " 1>&2
+	echo "Warning: data are being upgraded to an unstable database format."
+	echo "         This format may not be portable to newer stable release"
+	echo -n "Type \"continue\" to proceed: "
 	read answer
 
 	if [ $answer != "continue" ]; then
-		echo "Upgrade canceled" 1>&2
+		echo "Upgrade canceled"
 		exit 1
 	fi
+
+	echo
 fi
 
-while (( version < $DATABASE_VERSION )); do
-	if [ $TEERANK_VERBOSE -eq 1 ]; then
-        	echo "Upgrading from $version to $((version + 1))" 1>&2
-	fi
+backup="teerank.$version.upgrade-backup.tar.gz"
 
-	teerank-upgrade-$version-to-$(($version + 1)) || exit 1
+echo "Creating backup \"$backup\""
+tar czf "$backup" "$TEERANK_ROOT"
+
+while (( version < $DATABASE_VERSION )); do
+        echo "Upgrading from $version to $((version + 1))..."
+
+	teerank-upgrade-$version-to-$(($version + 1)) || {
+		echo "Error while upgrading from $version to $(($version + 1))"
+		echo "Restoring database with backup \"$backup\""
+
+		# We could use rm -r but I'm actually afraid of any
+		# mistake that will delete the wrong directory
+		tar xzf "$backup"
+		rm "$backup"
+
+		exit 1
+	}
 
 	((version++))
 	echo "$version" > "$file"
 done
+
+echo "Success! Removing backup"
+rm "$backup"

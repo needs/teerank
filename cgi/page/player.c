29132618e91313d7bd2885bb24a8122ff477d394
Needs <needs@mailoo.org>
2016-07-09 17:22:39 +0200

    Use player_summary in player page
    Player page do not need full historic

--- cgi/page/player.c
+++ cgi/page/player.c
@@ -12,7 +12,7 @@ int page_player_main(int argc, char **argv)
 {
 	char name[HEXNAME_LENGTH], clan[NAME_LENGTH];
 	const char *hexname;
-	struct player player;
+	struct player_summary player;
 	enum read_player_ret ret;
 
 	if (argc != 2) {
@@ -26,8 +26,7 @@ int page_player_main(int argc, char **argv)
 		return EXIT_NOT_FOUND;
 	}
 
-	init_player(&player);
-	ret = read_player(&player, hexname);
+	ret = read_player_summary(&player, hexname);
 
 	if (ret == PLAYER_ERROR)
 		return EXIT_FAILURE;

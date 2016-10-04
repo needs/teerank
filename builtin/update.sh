a79613841cb766574395897b4546019f3c48204e
Needs <needs@mailoo.org>
2016-10-04 15:00:46 +0200

    Rename compute-rank to build-indexes
    Even tho build-indexes still compute players rank, it can be conbsidered
    as a desired side effect of building indexes.

--- builtin/update.sh
+++ builtin/update.sh
@@ -6,4 +6,4 @@ teerank-init
 teerank-add-new-servers
 teerank-remove-offline-servers 1
 teerank-update-servers | teerank-update-players | teerank-update-clans
-teerank-compute-ranks
+teerank-build-indexes

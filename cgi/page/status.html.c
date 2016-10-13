01854bc1cc26d473fe38e5e8179faea6b8c8dad0
Needs <needs@mailoo.org>
2016-10-13 19:23:31 +0200

    Display masters status in status page
    Also this commit fix JSON API status by make it the same than Teerank
    status.
    
    With this commit, I think the status page is done now.

--- cgi/page/status.html.c
+++ cgi/page/status.html.c
@@ -65,36 +65,66 @@ int page_status_html_main(int argc, char **argv)
 {
 	char buf[16], comment[64];
 	struct info info;
+	short teerank_stopped = 0;
+	unsigned i;
 
 	if (argc != 1) {
 		fprintf(stderr, "usage: %s\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 
-	/* Eventually, print them */
 	CUSTOM_TAB.name = "Status";
 	CUSTOM_TAB.href = "";
 	html_header(&CUSTOM_TAB, "Status", NULL, NULL);
 
 	html("<h2>Teerank</h2>");
 
-	/* Teerank status */
+	/*
+	 * Teerank status, since the whole status is in the info struct,
+	 * failing to read it is fatal.
+	 */
 	if (!read_info(&info)) {
 		print_status("Teerank", "Can't read last update date", STATUS_UNKNOWN);
-	} else if (!elapsed_time_since(&info.last_update, NULL, buf, sizeof(buf))) {
-		snprintf(comment, sizeof(comment), "Not updated since %s", buf);
-		print_status("Teerank", comment, STATUS_STOPPED);
-	} else {
-		print_status("Teerank", NULL, STATUS_OK);
+		html_footer(NULL, NULL);
+		return EXIT_SUCCESS;
 	}
 
-	print_status("JSON API", NULL, STATUS_DISABLED);
+	if (!elapsed_time_since(&info.last_update, NULL, buf, sizeof(buf))) {
+		snprintf(comment, sizeof(comment), "Not updated since %s", buf);
+		print_status("Teerank", comment, STATUS_STOPPED);
+		print_status("JSON API", comment, STATUS_STOPPED);
+		teerank_stopped = 1;
+	} else {
+		print_status("Teerank", NULL, STATUS_OK);
+		print_status("JSON API", NULL, STATUS_OK);
+	}
 
-	html("<h2>Teeworlds</h2>");
-	print_status("master1.teeworlds.com", NULL, STATUS_DOWN);
-	print_status("master2.teeworlds.com", NULL, STATUS_OK);
-	print_status("master3.teeworlds.com", NULL, STATUS_OK);
-	print_status("master4.teeworlds.com", NULL, STATUS_UNKNOWN);
+
+	if (info.nmasters)
+		html("<h2>Teeworlds</h2>");
+
+	for (i = 0; i < info.nmasters; i++) {
+		struct master_info *minfo = &info.masters[i];
+
+		/*
+		 * If teerank is stopped, then we can't really guess
+		 * masters status.
+		 */
+		if (teerank_stopped)
+			print_status(minfo->node, NULL, STATUS_UNKNOWN);
+
+		else if (minfo->lastseen == NEVER_SEEN)
+			print_status(minfo->node, NULL, STATUS_DOWN);
+
+		else if (!elapsed_time_since(gmtime(&minfo->lastseen), NULL, buf, sizeof(buf))) {
+			snprintf(comment, sizeof(comment), "Since %s", buf);
+			print_status(minfo->node, comment, STATUS_DOWN);
+
+		} else {
+			snprintf(comment, sizeof(comment), "%u servers", minfo->nservers);
+			print_status(minfo->node, comment, STATUS_OK);
+		}
+	}
 
 	html_footer(NULL, NULL);
 

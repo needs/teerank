88b71e05984891b974954f394b6d4999224bfa49
Needs <needs@mailoo.org>
2016-10-22 01:33:41 +0200

    Don't use sprintf() to convert hexa to decimal

--- cgi/route.c
+++ cgi/route.c
@@ -26,6 +26,35 @@ struct url {
 	struct arg args[MAX_ARGS];
 };
 
+static unsigned char hextodec(char c)
+{
+	switch (c) {
+	case '0': return 0;
+	case '1': return 1;
+	case '2': return 2;
+	case '3': return 3;
+	case '4': return 4;
+	case '5': return 5;
+	case '6': return 6;
+	case '7': return 7;
+	case '8': return 8;
+	case '9': return 9;
+	case 'A':
+	case 'a': return 10;
+	case 'B':
+	case 'b': return 11;
+	case 'C':
+	case 'c': return 12;
+	case 'D':
+	case 'd': return 13;
+	case 'E':
+	case 'e': return 14;
+	case 'F':
+	case 'f': return 15;
+	default: return 0;
+	}
+}
+
 static void url_decode(char *str)
 {
 	char *tmp = str;
@@ -33,7 +62,11 @@ static void url_decode(char *str)
 	while (*str) {
 		if (*str == '%') {
 			unsigned byte;
-			sscanf(str + 1, "%2x", &byte);
+
+			if (str[1] == '\0' || str[2] == '\0')
+				break;
+
+			byte = hextodec(str[1]) * 16 + hextodec(str[2]);
 			*tmp = *(char*)&byte;
 			str += 3;
 		} else {

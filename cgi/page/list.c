e3ee79a4fb693c58119754220fadf96ac22324a1
Needs <needs@mailoo.org>
2017-05-12 16:26:46 +0200

    Merge list.{html,json}.c into list.c
    Those two files shared quite a bit of code, merging them actually saved
    40 lines and avoid some nasty code duplication.  Making changes to list
    will be easier in the futur because evrything will be in the same file.
    
    I'm looking forward to do the same things for other pages with a JSON
    counterpart.

--- cgi/page/list.c
+++ cgi/page/list.c
@@ -2,23 +2,50 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-#include <limits.h>
-#include <dirent.h>
-#include <errno.h>
+#include <stdbool.h>
 
 #include "cgi.h"
 #include "teerank.h"
 #include "html.h"
+#include "json.h"
 #include "database.h"
 #include "player.h"
 #include "clan.h"
 #include "server.h"
+
+static bool json = false;
 
 /* Global context shared by every lists */
 static char *gametype;
 static char *map;
 static char *order;
 static unsigned pnum;
+
+static void json_start_player_list(void)
+{
+	printf("{\"players\":[");
+}
+
+static void json_player_list_entry(unsigned nrow, struct player *p)
+{
+	if (nrow)
+		putchar(',');
+
+	putchar('{');
+	printf("\"name\":\"%s\",", json_hexstring(p->name));
+	printf("\"clan\":\"%s\",", json_hexstring(p->clan));
+	printf("\"elo\":%d,", p->elo);
+	printf("\"rank\":%u,", p->rank);
+	printf("\"lastseen\":\"%s\",", json_date(p->lastseen));
+	printf("\"server_ip\":\"%s\",", p->server_ip);
+	printf("\"server_port\":\"%s\"", p->server_port);
+	putchar('}');
+}
+
+static void json_end_player_list(unsigned nrow)
+{
+	printf("],\"length\":%u}", nrow);
+}
 
 static int print_player_list(void)
 {
@@ -38,22 +65,58 @@ static int print_player_list(void)
 		sortby = SORT_BY_RANK;
 	else if (strcmp(order, "lastseen") == 0)
 		sortby = SORT_BY_LASTSEEN;
-	else
+	else {
+		fprintf(stderr, "%s: Should be either \"rank\" or \"lastseen\"\n", order);
 		return EXIT_FAILURE;
+	}
 
-	html_start_player_list(relurl("/players/%s/%s", gametype, map), pnum, order);
 
 	snprintf(query, sizeof(query), queryfmt, sortby, (pnum - 1) * 100);
-	foreach_player(query, &player, "ss", gametype, map)
-		html_player_list_entry(&player, NULL, 0);
+
+	if (json)
+		json_start_player_list();
+	else
+		html_start_player_list(relurl("/players/%s/%s", gametype, map), pnum, order);
+
+	foreach_player(query, &player, "ss", gametype, map) {
+		if (json)
+			json_player_list_entry(nrow, &player);
+		else
+			html_player_list_entry(&player, NULL, 0);
+	}
 
 	if (!res)
 		return EXIT_FAILURE;
 	if (!nrow && pnum > 1)
 		return EXIT_NOT_FOUND;
 
-	html_end_player_list();
+	if (json)
+		json_end_player_list(nrow);
+	else
+		html_end_player_list();
+
 	return EXIT_SUCCESS;
+}
+
+static void json_start_clan_list(void)
+{
+	printf("{\"clans\":[");
+}
+
+static void json_clan_list_entry(unsigned nrow, struct clan *clan)
+{
+	if (nrow)
+		putchar(',');
+
+	putchar('{');
+	printf("\"name\":\"%s\",", json_hexstring(clan->name));
+	printf("\"nmembers\":\"%u\"", clan->nmembers);
+	putchar('}');
+}
+
+static void json_end_clan_list(unsigned nrow)
+{
+	printf("],\"length\":%u}", nrow);
 }
 
 static int print_clan_list(void)
@@ -70,19 +133,57 @@ static int print_clan_list(void)
 		" ORDER BY nmembers DESC, clan"
 		" LIMIT 100 OFFSET ?";
 
-	html_start_clan_list(relurl("/clans/%s/%s", gametype, map), pnum, NULL);
+	if (json)
+		json_start_clan_list();
+	else
+		html_start_clan_list(relurl("/clans/%s/%s", gametype, map), pnum, NULL);
 
 	offset = (pnum - 1) * 100;
-	foreach_clan(query, &clan, "u", offset)
-		html_clan_list_entry(++offset, clan.name, clan.nmembers);
+	foreach_clan(query, &clan, "u", offset) {
+		if (json)
+			json_clan_list_entry(nrow, &clan);
+		else
+			html_clan_list_entry(++offset, clan.name, clan.nmembers);
+	}
 
 	if (!res)
 		return EXIT_FAILURE;
 	if (!nrow && pnum > 1)
 		return EXIT_NOT_FOUND;
 
-	html_end_clan_list();
+	if (json)
+		json_end_clan_list(nrow);
+	else
+		html_end_clan_list();
+
 	return EXIT_SUCCESS;
+}
+
+static void json_start_server_list(void)
+{
+	printf("{\"servers\":[");
+}
+
+static void json_server_list_entry(unsigned nrow, struct server *server)
+{
+	if (nrow)
+		putchar(',');
+
+	putchar('{');
+	printf("\"ip\":\"%s\",", server->ip);
+	printf("\"port\":\"%s\",", server->port);
+	printf("\"name\":\"%s\",", json_escape(server->name));
+	printf("\"gametype\":\"%s\",", json_escape(server->gametype));
+	printf("\"map\":\"%s\",", json_escape(server->map));
+
+	printf("\"maxplayers\":%u,", server->max_clients);
+	printf("\"nplayers\":%u", server->num_clients);
+	putchar('}');
+}
+
+static void json_end_server_list(unsigned nrow)
+{
+	printf("],\"length\":%u}", nrow);
 }
 
 static int print_server_list(void)
@@ -98,18 +199,29 @@ static int print_server_list(void)
 		" ORDER BY num_clients DESC"
 		" LIMIT 100 OFFSET ?";
 
-	html_start_server_list(relurl("/servers/%s/%s", gametype, map), pnum, NULL);
+	if (json)
+		json_start_server_list();
+	else
+		html_start_server_list(relurl("/servers/%s/%s", gametype, map), pnum, NULL);
 
 	offset = (pnum - 1) * 100;
-	foreach_extended_server(query, &server, "u", offset)
-		html_server_list_entry(++offset, &server);
+	foreach_extended_server(query, &server, "u", offset) {
+		if (json)
+			json_server_list_entry(nrow, &server);
+		else
+			html_server_list_entry(++offset, &server);
+	}
 
 	if (!res)
 		return EXIT_FAILURE;
 	if (!nrow && pnum > 1)
 		return EXIT_NOT_FOUND;
 
-	html_end_server_list();
+	if (json)
+		json_end_server_list(nrow);
+	else
+		html_end_server_list();
+
 	return EXIT_SUCCESS;
 }
 
@@ -119,6 +231,8 @@ int main_html_list(struct url *url)
 	char *urlfmt;
 	unsigned tabvals[3];
 	int ret = EXIT_FAILURE;
+
+	json = false;
 
 	ret = parse_list_args(url, &pcs, &gametype, &map, &order, &pnum);
 	if (ret != EXIT_SUCCESS)
@@ -162,4 +276,27 @@ int main_html_list(struct url *url)
 	html_footer("player-list", relurl("/players/%s.json?p=%u", gametype, pnum));
 
 	return ret;
+}
+
+int main_json_list(struct url *url)
+{
+	enum pcs pcs;
+	int ret;
+
+	json = true;
+
+	ret = parse_list_args(url, &pcs, &gametype, &map, &order, &pnum);
+	if (ret != EXIT_SUCCESS)
+		return ret;
+
+	switch (pcs) {
+	case PCS_PLAYER:
+		return print_player_list();
+	case PCS_CLAN:
+		return print_clan_list();
+	case PCS_SERVER:
+		return print_server_list();
+	}
+
+	return EXIT_FAILURE;
 }

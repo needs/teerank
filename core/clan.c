63b7161f8ff9a6d6160c1fed37f745d5ab1116a5
Needs <needs@mailoo.org>
2017-06-19 14:03:31 +0200

    clan: Remove unused code

--- core/clan.c
+++ core/clan.c
@@ -3,14 +3,7 @@
 
 #include "clan.h"
 #include "teerank.h"
-
-void read_clan(sqlite3_stmt *res, void *_c)
-{
-	struct clan *c = _c;
-
-	snprintf(c->name, sizeof(c->name), "%s", sqlite3_column_text(res, 0));
-	c->nmembers = sqlite3_column_int64(res, 1);
-}
+#include "database.h"
 
 unsigned count_clans(void)
 {
@@ -23,7 +16,7 @@ unsigned count_clans(void)
 		" FROM"
 		" (SELECT DISTINCT clan"
 		"  FROM players"
-		"  WHERE" IS_VALID_CLAN ")";
+		"  WHERE clan IS NOT NULL)";
 
 	return count_rows(query);
 }
--- core/clan.h
+++ core/clan.h
@@ -1,23 +1,6 @@
 #ifndef CLAN_H
 #define CLAN_H
 
-#include "player.h"
-
-struct clan {
-	char name[NAME_LENGTH];
-	unsigned nmembers;
-};
-
-#define ALL_CLAN_COLUMNS " clan, COUNT(1) AS nmembers "
-
-#define IS_VALID_CLAN \
-	" clan <> '' "
-
-void read_clan(sqlite3_stmt *res, void *c);
-
-#define foreach_clan(query, m, ...) \
-	foreach_row(query, read_clan, m, __VA_ARGS__)
-
 unsigned count_clans(void);
 
 #endif /* CLAN_H */

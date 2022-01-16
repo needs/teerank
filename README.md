ef09bdbd2872a57376f33bd9ae6b6da75fbc6465
Timothée Vialatoux <timothee.vialatoux@dumblecore.com>
2022-01-16 18:37:16 +0100

    Launch a development flask server when running the test compose file.
    Once this development flask server is launched, no need to re-run the test
    compose file, waiting for the tests to finish.  Instead just refresh the page
    on your browser and voila!
    
    This doesn't work for the backend container.

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ lastest stable version at teerank.io.
 $ docker compose up --build
 
 # Test
-$ docker compose -f docker-compose-test.yml up --build --abort-on-container-exit
+$ docker compose -f docker-compose-test.yml up --build
 ```
 
 # Legacy
--- test/conftest.py
+++ test/conftest.py
@@ -3,6 +3,7 @@ Common fixtures for tests.
 """
 
 import pytest_redis
+from shared.database import redis
 
 redis_instance = pytest_redis.factories.redis_noproc(
     host='database-test'
@@ -11,3 +12,9 @@ redis_instance = pytest_redis.factories.redis_noproc(
 _redis = pytest_redis.factories.redisdb(
     'redis_instance'
 )
+
+def pytest_sessionfinish(session, exitstatus): # pylint: disable=unused-argument
+    """
+    Shutdown redis server once all tests have been run.
+    """
+    redis.shutdown()

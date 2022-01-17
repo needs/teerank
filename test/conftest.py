426d48710646c72ea58cd9e8ae1b20670702fd97
Timothée Vialatoux <timothee.vialatoux@dumblecore.com>
2022-01-17 16:30:13 +0100

    Move flask test server into a new docker compose file.

--- test/conftest.py
+++ test/conftest.py
@@ -3,7 +3,6 @@ Common fixtures for tests.
 """
 
 import pytest_redis
-from shared.database import redis
 
 redis_instance = pytest_redis.factories.redis_noproc(
     host='database-test'
@@ -12,9 +11,3 @@ redis_instance = pytest_redis.factories.redis_noproc(
 _redis = pytest_redis.factories.redisdb(
     'redis_instance'
 )
-
-def pytest_sessionfinish(session, exitstatus): # pylint: disable=unused-argument
-    """
-    Shutdown redis server once all tests have been run.
-    """
-    redis.shutdown()

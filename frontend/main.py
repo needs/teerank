3ff0140787a65f9dd85adf9a578ce995b8d56496
Timothée Vialatoux <timothee.vialatoux@dumblecore.com>
2022-01-18 23:01:12 +0100

    Implement a very basic /gametypes page.
    This page is just a dummy array that needs to be filled with real values later
    on.

--- frontend/main.py
+++ frontend/main.py
@@ -10,8 +10,14 @@ from shared.database import key_to_string, key_from_string
 
 app = Flask(__name__)
 
+main_game_types = (
+    'CTF',
+    'DM',
+    'TDM'
+)
+
 @app.route("/players")
-def route_players():
+def players():
     """
     List players for a specific game type and map.
     """
@@ -22,45 +28,72 @@ def route_players():
     game_type_key = key_from_string(game_type)
     map_name_key = key_from_string(map_name)
 
-    main_game_types = (
-        'CTF',
-        'DM',
-        'TDM'
-    )
-
     players_count = shared.player.players_count(game_type_key, map_name_key)
     players_key_elo = shared.player.players_by_rank(game_type_key, map_name_key, 0, 100)
-    players = []
+    players_list = []
 
     for rank, key_elo in enumerate(players_key_elo):
         key = key_elo[0].decode()
         elo = key_elo[1]
 
-        players.append({
+        players_list.append({
             'rank': rank,
             'name': key_to_string(key),
             'elo': int(elo)
         })
 
     return render_template(
-        'base.html',
+        'players.html',
         game_type=game_type,
         map_name=map_name,
-        players=players,
+        players=players_list,
         players_count=players_count,
         main_game_types=main_game_types
     )
 
 
+@app.route('/clans')
+def clans():
+    """
+    List of maps for a given game type.
+    """
+
+
+@app.route('/servers')
+def servers():
+    """
+    List of maps for a given game type.
+    """
+
+
 @app.route('/maps')
-def route_maps():
+def maps():
     """
     List of maps for a given game type.
     """
 
 
 @app.route('/gametypes')
-def route_gametypes():
+def gametypes():
     """
     List of all game types.
     """
+
+    game_types_list = [
+        {
+            'rank': 1,
+            'name': 'CTF',
+            'players': shared.player.players_count(key_from_string('CTF'), None),
+            'clans': 14,
+            'servers': 10,
+            'maps': 21
+        }
+    ]
+
+    return render_template(
+        'gametypes.html',
+        game_type=None,
+        map_name=None,
+        game_types=game_types_list,
+        main_game_types=main_game_types
+    )

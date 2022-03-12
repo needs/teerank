9197f5e57a703359300349a8486b38a7d3da6485
Timothée Vialatoux <timothee.vialatoux@dumblecore.com>
2022-03-12 14:05:12 +0100

    Remove unused code in shared.player

--- shared/player.py
+++ shared/player.py
@@ -7,81 +7,41 @@ from gql import gql
 from shared.database import redis, graphql
 
 
-class Player:
-    """
-    Teerank player.
-    """
-
-    def __init__(self, key: str):
-        """
-        Load player with the given key.
-        """
-        self.key = key
-
-        data = redis.get(f'players:{key}')
-
-        if data:
-            self.data = json.loads(data)
-        else:
-            self.data = None
-
-
-    def save(self) -> None:
-        """
-        Save player data.
-        """
-        redis.set(f'players:{self.key}', json.dumps(self.data))
-
-
-    @staticmethod
-    def get_elo(player_key: str, game_type: str, map_name: str) -> int:
-        """
-        Load player ELO for the given game type.
-        """
-
-        elo = redis.zscore(_key(game_type, map_name), player_key)
-        return elo if elo else 1500
-
-
-    @staticmethod
-    def set_elo(player_key: str, game_type: str, map_name: str, elo: int) -> None:
-        """
-        Save player ELO for the given game type.
-        """
-
-        redis.zadd(_key(game_type, map_name), {player_key: elo})
-
-
-def _key(game_type: str, map_name: str) -> str:
+def _key(game_type: str, map: str) -> str:
     """
     Return redis sorted set key for the given game type and map.
     """
 
-    if game_type is None and map_name is None:
-        return 'rank'
-    if game_type is not None and map_name is None:
-        return f'rank-gametype:{game_type}'
-    if game_type is None and map_name is not None:
-        return f'rank-map:{map_name}'
+    game_type_key = b64encode(game_type)
+    map_key = b64encode(map)
 
-    return f'rank-gametype-map:{game_type}:{map_name}'
+    if not game_type:
+        if not map:
+            return 'rank'
+        else:
+            return f'rank-map:{map_key}'
+    else:
+        if not map:
+            return f'rank-gametype:{game_type_key}'
+        else:
+            return f'rank-gametype-map:{game_type_key}:{map_key}'
 
 
-def players_by_rank(game_type: str, map_name: str, rank: int, count: int) -> list[str]:
+def get_elo(name: str, game_type: str, map: str) -> int:
     """
-    Return the number of players requested from the starting rank for the given
-    game type and map.
-    """
-
-    return redis.zrange(_key(game_type, map_name), rank, rank + count - 1, withscores=True)
-
-
-def players_count(game_type: str, map_name: str) -> int:
-    """
-    Return the number of players for the given game type and map.
+    Load player ELO for the given game type and map.
     """
 
-    return redis.zcard(_key(game_type, map_name))
+    elo = redis.zscore(_key(game_type, map), name)
+    return elo if elo else 1500
+
+
+def set_elo(name: str, game_type: str, map: str, elo: int) -> None:
+    """
+    Save player ELO for the given game type.
+    """
+
+    redis.zadd(_key(game_type, map), {name: elo})
 
 
 def ref(name):

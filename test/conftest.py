2b7c017a6322c1c238e25d9e1e82718713da7132
Timothée Vialatoux <timothee.vialatoux@dumblecore.com>
2022-03-15 20:38:31 +0100

    Add a _graphql ficture that initialize dgraph database for each tests .
    Like _redis fixture, it initialize the dgraph database at the beginning of the
    test session, and proceed to empty the database before each test.

--- test/conftest.py
+++ test/conftest.py
@@ -3,6 +3,9 @@ Common fixtures for tests.
 """
 
 import pytest_redis
+import pytest
+
+import shared.database
 
 redis_instance = pytest_redis.factories.redis_noproc(
     host='redis-test'
@@ -11,3 +14,19 @@ redis_instance = pytest_redis.factories.redis_noproc(
 _redis = pytest_redis.factories.redisdb(
     'redis_instance'
 )
+
+@pytest.fixture(scope='session')
+def fixture_graphql_start():
+    """
+    Prepare Dgraph to accept queries.
+    """
+    shared.database.graphql_set_schema()
+
+
+@pytest.fixture(name='_graphql')
+def fixture_graphql():
+    """
+    Empty graphql before each test.
+    """
+    shared.database.graphql_drop_all_data()
+    yield

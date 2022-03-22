57978a1047251c2e27c0dc4457d1e4d460058cf2
Timothée Vialatoux <timothee.vialatoux@dumblecore.com>
2022-03-22 20:29:05 +0100

    Test that status page display master server address.

--- test/frontend/test_status.py
+++ test/frontend/test_status.py
@@ -2,8 +2,15 @@
 Test /status.
 """
 
+import backend.database.master_server
+
 def test_status(client):
     """
     Test /status.
     """
-    assert client.get('/status').status_code == 200
+    address = 'foo:8080'
+    backend.database.master_server.create(address)
+    response = client.get('/status')
+
+    assert response.status_code == 200
+    assert address.encode() in response.data

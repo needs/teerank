f9968e36de60e6bc7e388fb0820f5532322228c6
Timothée Vialatoux <timothee.vialatoux@gmail.com>
2023-10-31 05:07:08 +0100

    Poll and rank every 5 minutes

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -7,9 +7,23 @@ import { updateGameTypesRankMethod } from './updateGameTypesRankMethod';
 async function main() {
   await addDefaultMasterServers();
   await updateGameTypesRankMethod();
-  await pollMasterServers();
-  await pollGameServers();
-  await rankPlayers();
+
+  let isRunning = false;
+
+  const callback = async () => {
+    if (!isRunning) {
+      isRunning = true;
+      await pollMasterServers();
+      await pollGameServers();
+      await rankPlayers();
+      isRunning = false;
+    } else {
+      console.warn('Skipping poll because it is already running');
+    }
+  };
+
+  callback();
+  setInterval(callback, 1000 * 60 * 5);
 }
 
 main();

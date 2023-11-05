aff211429c0f4bd951a5b09c4303a05da30c61c7
Timothée Vialatoux <timothee.vialatoux@gmail.com>
2023-11-05 22:36:50 +0100

    Start sentry only on production

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -8,20 +8,22 @@ import { prisma } from "./prisma";
 import * as Sentry from "@sentry/node";
 import { ProfilingIntegration } from "@sentry/profiling-node";
 
-Sentry.init({
-  dsn: 'https://ed10e9ae17c49680ba4ac68048b7a615@o4506165654192128.ingest.sentry.io/4506165660286976',
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: 'https://ed10e9ae17c49680ba4ac68048b7a615@o4506165654192128.ingest.sentry.io/4506165660286976',
 
-  integrations: [
-    new ProfilingIntegration(),
-    new Sentry.Integrations.Prisma({ client: prisma }),
-    new Sentry.Integrations.Console(),
-    new Sentry.Integrations.Mysql(),
-  ],
-  // Performance Monitoring
-  tracesSampleRate: 1.0,
-  // Set sampling rate for profiling - this is relative to tracesSampleRate
-  profilesSampleRate: 1.0,
-});
+    integrations: [
+      new ProfilingIntegration(),
+      new Sentry.Integrations.Prisma({ client: prisma }),
+      new Sentry.Integrations.Console(),
+      new Sentry.Integrations.Mysql(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0,
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: 1.0,
+  });
+}
 
 async function main() {
   await removeBadIps();

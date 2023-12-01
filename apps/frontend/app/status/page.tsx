47fbba8c898579cf7a4295d330602f962157c00c
Timothée Vialatoux <timothee.vialatoux@gmail.com>
2023-12-01 12:02:35 +0100

    Force dynamic rendering of status page

--- apps/frontend/app/status/page.tsx
+++ apps/frontend/app/status/page.tsx
@@ -8,6 +8,8 @@ export const metadata = {
   description: 'Teerank status',
 };
 
+export const dynamic = 'force-dynamic';
+
 export default async function Index() {
   const tasks = await prisma.task.findMany({
     select: {

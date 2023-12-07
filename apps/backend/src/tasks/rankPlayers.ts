90969db8f16e232bdd2e39d4555aabaaab8b81a0
Timothée Vialatoux <timothee.vialatoux@gmail.com>
2023-12-07 16:05:25 +0100

    Speed up player rank loading

--- apps/backend/src/tasks/rankPlayers.ts
+++ apps/backend/src/tasks/rankPlayers.ts
@@ -4,6 +4,7 @@ import { fromBase64, toBase64 } from "../utils";
 import groupBy from "lodash.groupby";
 import { rankPlayersElo } from "../rankMethods/rankPlayersElo";
 import { rankPlayersTime } from "../rankMethods/rankPlayersTime";
+import { set } from "date-fns";
 
 export type SnapshotToRank = Awaited<ReturnType<typeof getGameServers>>[number]['snapshots'][number];
 
@@ -95,42 +96,50 @@ export const rankPlayers = async () => {
     console.time(`Loading player infos by game type`);
   }
 
-  const playerInfosByGameType = await prisma.$transaction(
-    gameServers.flatMap((gameServer) =>
-      gameServer.snapshots.flatMap((snapshot) =>
-        snapshot.clients.map((client) =>
-          prisma.playerInfo.upsert({
-            where: {
-              playerName_gameTypeName: {
-                gameTypeName: snapshot.map.gameTypeName,
-                playerName: client.playerName,
-              },
-            },
-            select: {
-              rating: true,
-              playerName: true,
-              gameTypeName: true,
-            },
-            update: {},
-            create: {
-              gameTypeName: snapshot.map.gameTypeName,
-              playerName: client.playerName,
-            },
-          })
-        )
-      )
-    ),
-  );
-
-  const ratingByGameType = new Map<string, number>();
+  const ratingByGameType = new Map<string, number | undefined>();
 
   const gameTypeKey = (gameTypeName: string, playerName: string) => `${toBase64(gameTypeName)}:${toBase64(playerName)}`;
   const getGameTypeRating = (gameTypeName: string, playerName: string) => ratingByGameType.get(gameTypeKey(gameTypeName, playerName));
-  const setGameTypeRating = (gameTypeName: string, playerName: string, rating: number) => ratingByGameType.set(gameTypeKey(gameTypeName, playerName), rating);
+  const setGameTypeRating = (gameTypeName: string, playerName: string, rating: number | undefined) => ratingByGameType.set(gameTypeKey(gameTypeName, playerName), rating);
+
+  for (const gameServer of gameServers) {
+    for (const snapshot of gameServer.snapshots) {
+      for (const client of snapshot.clients) {
+        setGameTypeRating(snapshot.map.gameTypeName, client.playerName, undefined);
+      }
+    }
+  }
+
+  const playerInfosByGameType = await prisma.$transaction(
+    [...ratingByGameType.keys()].map((key) => {
+      const [gameTypeNameEncoded, playerNameEncoded] = key.split(':');
+      const gameTypeName = fromBase64(gameTypeNameEncoded);
+      const playerName = fromBase64(playerNameEncoded);
+
+      return prisma.playerInfo.upsert({
+        where: {
+          playerName_gameTypeName: {
+            gameTypeName,
+            playerName,
+          },
+        },
+        select: {
+          rating: true,
+          playerName: true,
+          gameTypeName: true,
+        },
+        update: {},
+        create: {
+          gameTypeName,
+          playerName,
+        },
+      });
+    })
+  );
 
   for (const playerInfo of playerInfosByGameType) {
-    if (playerInfo.gameTypeName !== null && playerInfo.rating !== null) {
-      setGameTypeRating(playerInfo.gameTypeName, playerInfo.playerName, playerInfo.rating);
+    if (playerInfo.gameTypeName !== null) {
+      setGameTypeRating(playerInfo.gameTypeName, playerInfo.playerName, playerInfo.rating ?? undefined);
     }
   }
 
@@ -139,42 +148,51 @@ export const rankPlayers = async () => {
     console.time(`Loading player infos by map`);
   }
 
-  const playerInfosByMap = await prisma.$transaction(
-    gameServers.flatMap((gameServer) =>
-      gameServer.snapshots.flatMap((snapshot) =>
-        snapshot.clients.map((client) =>
-          prisma.playerInfo.upsert({
-            where: {
-              playerName_mapId: {
-                mapId: snapshot.mapId,
-                playerName: client.playerName,
-              },
-            },
-            select: {
-              rating: true,
-              playerName: true,
-              mapId: true,
-            },
-            update: {},
-            create: {
-              mapId: snapshot.mapId,
-              playerName: client.playerName,
-            },
-          })
-        )
-      )
-    ),
-  );
-
-  const ratingByMap = new Map<string, number>();
+  const ratingByMap = new Map<string, number | undefined>();
 
   const mapKey = (mapId: number, playerName: string) => `${mapId}:${toBase64(playerName)}`;
   const getMapRating = (mapId: number, playerName: string) => ratingByMap.get(mapKey(mapId, playerName));
-  const setMapRating = (mapId: number, playerName: string, rating: number) => ratingByMap.set(mapKey(mapId, playerName), rating);
+  const setMapRating = (mapId: number, playerName: string, rating: number | undefined) => ratingByMap.set(mapKey(mapId, playerName), rating);
+
+  for (const gameServer of gameServers) {
+    for (const snapshot of gameServer.snapshots) {
+      for (const client of snapshot.clients) {
+        setMapRating(snapshot.mapId, client.playerName, undefined);
+      }
+    }
+  }
+
+  const playerInfosByMap = await prisma.$transaction(
+    [...ratingByMap.keys()].map((key) => {
+      const [mapIdEncoded, playerNameEncoded] = key.split(':');
+      const mapId = Number(mapIdEncoded);
+      const playerName = fromBase64(playerNameEncoded);
+
+
+      return prisma.playerInfo.upsert({
+        where: {
+          playerName_mapId: {
+            mapId: mapId,
+            playerName: playerName,
+          },
+        },
+        select: {
+          rating: true,
+          playerName: true,
+          mapId: true,
+        },
+        update: {},
+        create: {
+          mapId: mapId,
+          playerName: playerName,
+        },
+      })
+    })
+  );
 
   for (const playerInfo of playerInfosByMap) {
-    if (playerInfo.mapId !== null && playerInfo.rating !== null) {
-      setMapRating(playerInfo.mapId, playerInfo.playerName, playerInfo.rating);
+    if (playerInfo.mapId !== null) {
+      setMapRating(playerInfo.mapId, playerInfo.playerName, playerInfo.rating ?? undefined);
     }
   }
 

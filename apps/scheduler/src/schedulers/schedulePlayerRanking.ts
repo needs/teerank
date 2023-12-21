30df57e3dcb9d1742054ec8edf730bbe0a7ee1d4
Timothée Vialatoux <timothee.vialatoux@gmail.com>
2023-12-21 17:33:14 +0100

    Reduce the number of queries when scheduling

--- apps/scheduler/src/schedulers/schedulePlayerRanking.ts
+++ apps/scheduler/src/schedulers/schedulePlayerRanking.ts
@@ -1,14 +1,11 @@
 import { JobType } from "@prisma/client";
 import { prisma } from "../prisma";
 import { MAXIMUM_JOB_PER_SCHEDULE, scheduleJobBatches } from "../schedule";
-import { compact} from 'lodash';
 
 const BATCH_SIZE = 100;
 
 export async function schedulePlayerRanking() {
-  const offsets = Array.from({ length: MAXIMUM_JOB_PER_SCHEDULE }, (_, index) => index * BATCH_SIZE);
-
-  const mostRecentUnrankedSnapshots = await prisma.$transaction(offsets.map(offset => prisma.gameServerSnapshot.findFirst({
+  const mostRecentUnrankedSnapshot = await prisma.gameServerSnapshot.findFirst({
     select: {
       id: true
     },
@@ -18,10 +15,10 @@ export async function schedulePlayerRanking() {
     orderBy: {
       createdAt: 'desc',
     },
-    skip: offset,
-  })));
+  });
 
-  const batches = compact(mostRecentUnrankedSnapshots.map(snapshot => snapshot?.id));
-
-  await scheduleJobBatches(JobType.RANK_PLAYERS, BATCH_SIZE, batches);
+  if (mostRecentUnrankedSnapshot !== null) {
+    const batches = Array.from({ length: MAXIMUM_JOB_PER_SCHEDULE }, (_, index) => mostRecentUnrankedSnapshot.id - index * BATCH_SIZE);
+    await scheduleJobBatches(JobType.RANK_PLAYERS, BATCH_SIZE, batches);
+  }
 }

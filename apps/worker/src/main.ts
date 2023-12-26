be32c705cb37586b92280bd58e14c277f595547e
Timothée Vialatoux <timothee.vialatoux@gmail.com>
2023-12-26 01:52:29 +0100

    Cleaner performances reports

--- apps/worker/src/main.ts
+++ apps/worker/src/main.ts
@@ -5,6 +5,7 @@ import { pollGameServers } from "./tasks/pollGameServers";
 import { pollMasterServers } from "./tasks/pollMasterServers";
 import { rankPlayers } from "./tasks/rankPlayers";
 import { updatePlayTimes } from "./tasks/updatePlayTime";
+import { reportPerformances, monitorJobPerformance } from "./tasks/reportPerformances";
 import { cancellableWait } from "./utils";
 
 let stopGracefully = false;
@@ -20,79 +21,11 @@ process.on('SIGINT', () => {
   cancelGracefully();
 });
 
-const jobPerformances = new Map<string, {
-  numberOfBusyRun: number;
-  totalBusyDuration: number;
-  maxBusyDuration: number;
-
-  numberOfIdleRun: number;
-  totalIdleDuration: number;
-  maxIdleDuration: number;
-}>();
-
-function formatDuration(duration: number) {
-  if (isNaN(duration)) {
-    return 0;
-  }
-
-  return Number(duration.toFixed(3));
-}
-
-async function printPerformanceSummary() {
-  const performanceSummary = [...jobPerformances.entries()].map(([jobName, performanceData]) => {
-    return {
-      jobName,
-
-      busyRun: performanceData.numberOfBusyRun,
-      busyAvg: formatDuration(performanceData.totalBusyDuration / performanceData.numberOfBusyRun),
-      busyMax: formatDuration(performanceData.maxBusyDuration),
-
-      idleRun: performanceData.numberOfIdleRun,
-      idleAvg: formatDuration(performanceData.totalIdleDuration / performanceData.numberOfIdleRun ?? undefined),
-      idleMax: formatDuration(performanceData.maxIdleDuration),
-    };
-  });
-
-  if (performanceSummary.length === 0) {
-    return false;
-  }
-
-  console.table(performanceSummary.filter(({ jobName }) => jobName !== 'printPerformanceSummary'));
-  jobPerformances.clear();
-
-  return true;
-}
-
 async function runJob(job: () => Promise<boolean>, jobName: string, delayOnBusy: number, delayOnIdle: number) {
   console.log(`Starting ${jobName}`);
 
   while (!stopGracefully) {
-    performance.mark(`${jobName}-start`);
-    const isBusy = await job();
-    performance.mark(`${jobName}-end`);
-    const measure = performance.measure(jobName, `${jobName}-start`, `${jobName}-end`);
-
-    const performanceData = jobPerformances.get(jobName) ?? {
-      numberOfBusyRun: 0,
-      totalBusyDuration: 0,
-      maxBusyDuration: 0,
-
-      numberOfIdleRun: 0,
-      totalIdleDuration: 0,
-      maxIdleDuration: 0,
-    };
-
-    if (isBusy) {
-      performanceData.numberOfBusyRun++;
-      performanceData.totalBusyDuration += measure.duration;
-      performanceData.maxBusyDuration = Math.max(performanceData.maxBusyDuration, measure.duration);
-    } else {
-      performanceData.numberOfIdleRun++;
-      performanceData.totalIdleDuration += measure.duration;
-      performanceData.maxIdleDuration = Math.max(performanceData.maxIdleDuration, measure.duration);
-    }
-
-    jobPerformances.set(jobName, performanceData);
+    const isBusy = await monitorJobPerformance(jobName, job);
 
     const { wait, cancel } = cancellableWait(isBusy ? delayOnBusy : delayOnIdle);
 
@@ -116,7 +49,7 @@ async function main() {
     runJob(pollGameServers, 'pollGameServers', 0, 5000),
     runJob(rankPlayers, 'rankPlayers', 0, 5000),
     runJob(updatePlayTimes, 'updatePlayTimes', 0, 5000),
-    runJob(printPerformanceSummary, 'printPerformanceSummary', 60000, 60000),
+    runJob(reportPerformances, 'reportPerformances', 60000, 60000),
   ]);
 
   console.log('Stopped gracefully');
---
+++ apps/worker/src/tasks/reportPerformances.ts
@@ -0,0 +1,76 @@
+const jobPerformances = new Map<string, {
+  numberOfBusyRun: number;
+  totalBusyDuration: number;
+  maxBusyDuration: number;
+
+  numberOfIdleRun: number;
+  totalIdleDuration: number;
+  maxIdleDuration: number;
+}>();
+
+export async function monitorJobPerformance(jobName: string, job: () => Promise<boolean>) {
+  if (job === reportPerformances) {
+    return await job();
+  }
+
+  performance.mark(`${jobName}-start`);
+  const isBusy = await job();
+  performance.mark(`${jobName}-end`);
+  const measure = performance.measure(jobName, `${jobName}-start`, `${jobName}-end`);
+
+  const performanceData = jobPerformances.get(jobName) ?? {
+    numberOfBusyRun: 0,
+    totalBusyDuration: 0,
+    maxBusyDuration: 0,
+
+    numberOfIdleRun: 0,
+    totalIdleDuration: 0,
+    maxIdleDuration: 0,
+  };
+
+  if (isBusy) {
+    performanceData.numberOfBusyRun++;
+    performanceData.totalBusyDuration += measure.duration;
+    performanceData.maxBusyDuration = Math.max(performanceData.maxBusyDuration, measure.duration);
+  } else {
+    performanceData.numberOfIdleRun++;
+    performanceData.totalIdleDuration += measure.duration;
+    performanceData.maxIdleDuration = Math.max(performanceData.maxIdleDuration, measure.duration);
+  }
+
+  jobPerformances.set(jobName, performanceData);
+  return isBusy;
+}
+
+function formatDuration(duration: number) {
+  if (isNaN(duration)) {
+    return 0;
+  }
+
+  return Number(duration.toFixed(3));
+}
+
+export async function reportPerformances() {
+  const performanceSummary = [...jobPerformances.entries()].map(([jobName, performanceData]) => {
+    return {
+      jobName,
+
+      busyRun: performanceData.numberOfBusyRun,
+      busyAvg: formatDuration(performanceData.totalBusyDuration / performanceData.numberOfBusyRun),
+      busyMax: formatDuration(performanceData.maxBusyDuration),
+
+      idleRun: performanceData.numberOfIdleRun,
+      idleAvg: formatDuration(performanceData.totalIdleDuration / performanceData.numberOfIdleRun ?? undefined),
+      idleMax: formatDuration(performanceData.maxIdleDuration),
+    };
+  });
+
+  if (performanceSummary.length === 0) {
+    return false;
+  }
+
+  console.table(performanceSummary);
+  jobPerformances.clear();
+
+  return true;
+}

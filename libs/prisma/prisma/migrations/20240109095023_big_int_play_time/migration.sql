399f352c6764828816d7de0523d4b2b715e0aa72
Timothée Vialatoux <timothee.vialatoux@gmail.com>
2024-01-09 10:51:15 +0100

    Use BigInt for playTime

---
+++ libs/prisma/prisma/migrations/20240109095023_big_int_play_time/migration.sql
@@ -0,0 +1,26 @@
+-- AlterTable
+ALTER TABLE "Clan" ALTER COLUMN "playTime" SET DATA TYPE BIGINT;
+
+-- AlterTable
+ALTER TABLE "ClanInfoGameType" ALTER COLUMN "playTime" SET DATA TYPE BIGINT;
+
+-- AlterTable
+ALTER TABLE "ClanInfoMap" ALTER COLUMN "playTime" SET DATA TYPE BIGINT;
+
+-- AlterTable
+ALTER TABLE "ClanPlayerInfo" ALTER COLUMN "playTime" SET DATA TYPE BIGINT;
+
+-- AlterTable
+ALTER TABLE "GameType" ALTER COLUMN "playTime" SET DATA TYPE BIGINT;
+
+-- AlterTable
+ALTER TABLE "Map" ALTER COLUMN "playTime" SET DATA TYPE BIGINT;
+
+-- AlterTable
+ALTER TABLE "Player" ALTER COLUMN "playTime" SET DATA TYPE BIGINT;
+
+-- AlterTable
+ALTER TABLE "PlayerInfoGameType" ALTER COLUMN "playTime" SET DATA TYPE BIGINT;
+
+-- AlterTable
+ALTER TABLE "PlayerInfoMap" ALTER COLUMN "playTime" SET DATA TYPE BIGINT;

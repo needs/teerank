6db6da7eb3344c460c5c7f764825d8fa25fa287d
Timothée Vialatoux <timothee.vialatoux@gmail.com>
2024-03-05 14:09:15 +0100

    Add playground

---
+++ scripts/playground.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+export const prismaDatabaseUrl = process.env.DATABASE_URL;
+
+export const prisma = new PrismaClient({
+  datasourceUrl: prismaDatabaseUrl,
+});
+
+async function main() {
+  // This is a playground to test the code.  Run it using `npm run playground`
+  // or `npm run playground-prod`.  Be careful with the code you write here if
+  // you are running it against prod.
+}
+
+main()

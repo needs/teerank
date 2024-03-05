ab871a0bec0c1f7aecdd33e136c66f85111692e9
Timothée Vialatoux <timothee.vialatoux@gmail.com>
2024-03-05 13:34:49 +0100

    Add index to better inspect faulty player scores

---
+++ libs/prisma/prisma/migrations/20240305123440_index_scores/migration.sql
@@ -0,0 +1,5 @@
+-- CreateIndex
+CREATE INDEX "GameServerClient_score_inGame_idx" ON "GameServerClient"("score", "inGame");
+
+-- CreateIndex
+CREATE INDEX "PlayerInfoMap_rating_idx" ON "PlayerInfoMap"("rating");

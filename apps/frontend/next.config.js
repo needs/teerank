28b57b2dca07847875e2552f42ef31524c76aa2e
Timothée Vialatoux <timothee.vialatoux@gmail.com>
2024-03-19 03:36:50 +0100

    Fix Next config to upload source maps for Sentry

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { withNx } = require('@nx/next');
-const { withSentryConfig } = require("@sentry/nextjs");
-
+const { withSentryConfig } = require('@sentry/nextjs');
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -13,14 +12,6 @@ const nextConfig = {
     svgr: false,
   },
   sentry: {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // Suppresses source map uploading logs during build
-    silent: true,
-    org: "teerank",
-    project: "frontend",
-
     // Hides source maps from generated client bundles
     hideSourceMaps: true,
 
@@ -33,18 +24,29 @@ const nextConfig = {
     // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers. (increases server load)
     // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
     // side errors will fail.
-    tunnelRoute: "/monitoring",
+    tunnelRoute: '/monitoring',
 
     // Automatically tree-shake Sentry logger statements to reduce bundle size
     disableLogger: true,
-  }
+  },
 };
 
 async function withCustom(phase) {
   const nxConfig = withNx(nextConfig);
   const c = await nxConfig(phase);
 
-  return withSentryConfig(c, {});
+  return withSentryConfig(c, {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+    org: 'teerank',
+    project: 'frontend',
+
+    // An auth token is required for uploading source maps.
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+  });
 }
 
 module.exports = withCustom;

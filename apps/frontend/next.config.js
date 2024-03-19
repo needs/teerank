26968995aa54aeb23d8fb4c0f869f88849ef8d7d
Timothée Vialatoux <timothee.vialatoux@gmail.com>
2024-03-19 03:29:18 +0100

    Integrate sentry for better monitoring

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,7 +1,7 @@
-//@ts-check
-
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { composePlugins, withNx } = require('@nx/next');
+const { withNx } = require('@nx/next');
+const { withSentryConfig } = require("@sentry/nextjs");
+
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
@@ -12,11 +12,39 @@ const nextConfig = {
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
+  sentry: {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+    org: "teerank",
+    project: "frontend",
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: true,
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers. (increases server load)
+    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+    // side errors will fail.
+    tunnelRoute: "/monitoring",
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+  }
 };
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-];
+async function withCustom(phase) {
+  const nxConfig = withNx(nextConfig);
+  const c = await nxConfig(phase);
 
-module.exports = composePlugins(...plugins)(nextConfig);
+  return withSentryConfig(c, {});
+}
+
+module.exports = withCustom;
---
+++ apps/frontend/sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://ac2be18a612e1eba61b4069c21d42ed7@o4506165654192128.ingest.us.sentry.io/4506935101947904",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});

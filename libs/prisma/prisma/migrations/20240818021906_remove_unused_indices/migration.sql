844cc0246c7424f9ba88db29cea7e9fd329d2d5b
Timothée Vialatoux <timothee.vialatoux@gmail.com>
2024-08-18 10:19:24 +0800

    Remove unused indices

---
+++ libs/prisma/prisma/migrations/20240818021906_remove_unused_indices/migration.sql
@@ -0,0 +1,23 @@
+-- DropIndex
+DROP INDEX "ClanInfoGameType_playTime_idx";
+
+-- DropIndex
+DROP INDEX "GameServer_createdAt_idx";
+
+-- DropIndex
+DROP INDEX "GameServerSnapshot_createdAt_playTimedAt_idx";
+
+-- DropIndex
+DROP INDEX "GameServerSnapshot_createdAt_rankedAt_idx";
+
+-- DropIndex
+DROP INDEX "GameServerSnapshot_createdAt_rankingStartedAt_idx";
+
+-- DropIndex
+DROP INDEX "GameType_playTime_idx";
+
+-- DropIndex
+DROP INDEX "Map_playTime_idx";
+
+-- DropIndex
+DROP INDEX "PlayerInfoGameType_playTime_idx";

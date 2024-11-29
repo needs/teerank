285e08e2ba6213a26e4cc6e252190e1e0eec271b
Timothée Vialatoux <timothee.vialatoux@gmail.com>
2024-11-29 17:38:11 +0100

    Add bull-board and use family: 6 for redis connection

--- apps/worker/src/bullmq.ts
+++ apps/worker/src/bullmq.ts
@@ -1,6 +1,8 @@
+import { RedisOptions } from "bullmq";
 import { REDIS_HOST, REDIS_PORT } from "./redis";
 
-export const bullmqConnection = {
+export const bullmqConnection: RedisOptions = {
   host: REDIS_HOST,
   port: REDIS_PORT,
+  family: 6,
 };

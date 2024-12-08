bbc90f2cd587ec135f9c9e5fbd8d3e41bb1a35b0
Timothée Vialatoux <timothee.vialatoux@gmail.com>
2024-12-08 05:30:52 +0100

    Remove search app

--- apps/search/src/app/routes/root.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance } from 'fastify';
-
-export default async function (fastify: FastifyInstance) {
-  fastify.get('/', async function () {
-    return { message: 'Hello API' };
-  });
-}

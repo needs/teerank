ce0750ac0966525c87cf7989945dc8f65f3280cb
Timothée Vialatoux <timothee.vialatoux@gmail.com>
2024-12-30 23:41:55 +0100

    Add Sentry to scheduler

--- apps/scheduler/src/utils.ts
+++ apps/scheduler/src/utils.ts
@@ -1,8 +1,16 @@
 import { wait } from "@teerank/teerank";
+import { captureException } from "@sentry/node";
 
 export function schedule(every: number, callback: () => Promise<void>) {
-  callback();
-  setInterval(callback, every);
+  const callbackWithSentry = () => {
+    callback().catch(error => {
+      console.error(error);
+      captureException(error);
+    });
+  }
+
+  callbackWithSentry();
+  setInterval(callbackWithSentry, every);
 }
 
 export function scheduleWithSpread(every: number, callback: () => Promise<void>) {

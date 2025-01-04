673f30dd0b25f4a0194b8416bd1eacc44cbd2286
Timothée Vialatoux <timothee.vialatoux@gmail.com>
2025-01-04 16:18:42 +0100

    Don't clean update play time queue and rank player queue

--- apps/scheduler/src/cleanQueues.ts
+++ apps/scheduler/src/cleanQueues.ts
@@ -2,18 +2,14 @@ import {
   cleanQueue,
   getQueueMapCount,
   getQueueGameTypeCount,
-  getQueueRankPlayer,
   getQueuePollGameServer,
   getQueuePollMasterServer,
-  getQueueUpdatePlayTime
 } from '@teerank/teerank';
 
 export async function cleanQueues() {
   await Promise.all([
     cleanQueue(getQueuePollMasterServer()),
     cleanQueue(getQueuePollGameServer()),
-    cleanQueue(getQueueUpdatePlayTime()),
-    cleanQueue(getQueueRankPlayer()),
     cleanQueue(getQueueGameTypeCount()),
     cleanQueue(getQueueMapCount())
   ]);
